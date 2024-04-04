d2b9f37463a501b4e33f41c85b593a925a56e7b9
Andriikryl <andreyk557@gmail.com>
2024-04-04 14:29:58 +0300

    add icons

---
+++ src/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as Insta } from "./Insta";
+export { default as Paint } from "./Paint";
+export { default as Twitter } from "./Twitter";
+export { default as FaceBooke } from "./FaceBook";
+export { default as Local } from "./Local";
+export { default as Hours } from "./Hours";
+export { default as Reservation } from "./Reservation";
